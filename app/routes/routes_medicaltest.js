6989da46196bdd83ebb5bf6dbc62f12d9e6d4638
sumaliJayasinghe <sukijayasinghe@gmail.com>
2018-05-08 14:34:14 +0800

    Add files via upload

--- app/routes/routes_medicaltest.js
+++ app/routes/routes_medicaltest.js
@@ -1,45 +1,68 @@
-var appRouter = function (app) {
+var appReportRouter = function(app, model) {
     app.get("/", function(req, res) {
-      res.status(200).send("Welcome to our restful API");
+        res.status(200).send("Welcome to our restful API");
     });
 
-      app.get("/310/healthReport/getMedicalTest", function (req, res) {
-        var data = ({
-          firstName: "sumali",
-          lastName: "jayasinghe",
-          username: "suki",
-          email: "sukijayasinghe@gmail.com"
-        });
-        res.status(200).send(data);
-      });
-      app.get("/310/healthReport/createMedicalTest", function (req, res) {
-        var data = ({
-          firstName: "sumali",
-          lastName: "jayasinghe",
-          username: "suki",
-          email: "sukijayasinghe@gmail.com"
-        });
-        res.status(200).send(data);
-      });
+    app.get("/310/healthReport/getMedicalTest", function(req, res) {
+        model.MedicalReport.findAll({}).then(test => res.json({
+                status: 200,
+                data: test
+            }))
+            .catch(error => res.json({
+                status: 203,
+                error: error
+            }));
+    });
+    app.get("/310/healthReport/getMedicalTest/:id", function(req, res) {
+        model.MedicalReport.findById(req.params.id).then(tests => res.json({
+                status: 200,
+                data: tests
+            }))
+            .catch(error => res.json({
+                status: 203,
+                error: error
+            }));
+    });
 
-      app.get("/310/healthReport/updateMedicalTest", function (req, res) {
-        var data = ({
-          firstName: "sumali",
-          lastName: "jayasinghe",
-          username: "suki",
-          email: "sukijayasinghe@gmail.com"
-        });
-        res.status(200).send(data);
-      });
+    app.post("/310/healthReport/createMedicalTest", function(req, res) {
+        model.MedicalReport.create(req.body).then(re => res.json({
+                status: 201,
+                data: re
+            }))
+            .catch(error => res.json({
+                status: 203,
+                error: error
+            }));
+    });
 
-      app.get("/310/healthReport/deleteMedicalTest", function (req, res) {
-        var data = ({
-          firstName: "sumali",
-          lastName: "jayasinghe",
-          username: "suki",
-          email: "sukijayasinghe@gmail.com"
-        });
-        res.status(200).send(data);
-      });
-  }
-module.exports = appRouter;  
+    app.update("/310/healthReport/updateMedicalTest/:id", function(req, res) {
+        model.MedicalReport.update(req.body, {
+                where: {
+                    id: req.params.id
+                }
+            })
+            .then(user => res.json({
+                status: 201,
+                data: user
+            }))
+            .catch(error => res.json({
+                status: 203,
+                error: error
+            }));
+    });
+    app.delete("/310/healthReport/deleteMedicalTest/:id", function(req, res) {
+        model.MedicalReport.destroy({
+                where: {
+                    id: req.params.id
+                }
+            }).then(resp => res.json({
+                status: 200,
+                data: resp
+            }))
+            .catch(error => res.json({
+                status: 203,
+                error: error
+            }));
+    });
+}
+module.exports = appReportRouter;
--- app/routes/routes_user.js
+++ app/routes/routes_user.js
@@ -1,41 +1,67 @@
-var appRouter = function (app) {
+var appUserRouter = function(app, model) {
     app.get("/", function(req, res) {
-      res.status(200).send("Welcome to our restful API");
+        res.status(200).send("Welcome to our restful API");
     });
 
-    app.get("/310/healthReport/getUser", function (req, res) {
-        var data = ({
-          firstName: "sumali",
-          lastName: "jayasinghe",
-          id: "8878234567V",
-          email: "sukijayasinghe@gmail.com",
-          age: "30",
-          weight:"57KG"
-        });
-        res.status(200).send(data);
-      });
-      app.get("/310/healthReport/createUser", function (req, res) {
-        var data = ({
-          firstName: "sumali",
-          lastName: "jayasinghe",
-          id: "8878234567V",
-          email: "sukijayasinghe@gmail.com",
-          age: "30",
-          weight:"57KG"
-        });
-        res.status(200).send(data);
-      });
+    app.get("/310/healthReport/getAllUser", function(req, res) {
+        model.User.findAll({}).then(users => res.json({
+                status: 200,
+                data: users
+            }))
+            .catch(error => res.json({
+                status: 203,
+                error: error
+            }));
+    });
+    app.get("/310/healthReport/getUser/:id", function(req, res) {
+        model.User.findById(req.params.id).then(user => res.json({
+                status: 200,
+                data: user
+            }))
+            .catch(error => res.json({
+                status: 203,
+                error: error
+            }));
+    });
+    app.post("/310/healthReport/createUser", function(req, res) {
+        model.User.create(req.body).then(user => res.json({
+                status: 201,
+                data: user
+            }))
+            .catch(error => res.json({
+                status: 203,
+                error: error
+            }));
+    });
 
-      app.get("/310/healthReport/updateUser", function (req, res) {
-        var data = ({
-          firstName: "sumali",
-          lastName: "jayasinghe",
-          id: "8878234567V",
-          email: "sukijayasinghe@gmail.com",
-          age: "30",
-          weight:"57KG"
-        });
-        res.status(200).send(data);
-      });
-  }
-module.exports = appRouter;  
+    app.update("/310/healthReport/updateUser/:id", function(req, res) {
+        model.User.update(req.body, {
+                where: {
+                    id: req.params.id
+                }
+            })
+            .then(user => res.json({
+                status: 201,
+                data: user
+            }))
+            .catch(error => res.json({
+                status: 203,
+                error: error
+            }));
+    });
+    app.delete("/310/healthReport/deleteUser/:id", function(req, res) {
+        model.User.destroy({
+                where: {
+                    id: req.params.id
+                }
+            }).then(resp => res.json({
+                status: 200,
+                data: resp
+            }))
+            .catch(error => res.json({
+                status: 203,
+                error: error
+            }));
+    });
+}
+module.exports = appUserRouter;

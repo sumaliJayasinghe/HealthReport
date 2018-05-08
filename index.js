b4402d8c48dfdcf190e7821e3b273b5c1f5b49ab
sumaliJayasinghe <sukijayasinghe@gmail.com>
2018-05-08 14:34:59 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var userRoutes = require("./routes/routes_user.js");
-var medTestRoutes = require("./routes/routes_medicaltest.js");
+var userRoutes = require("./app/routes/routes_user.js");
+var medTestRoutes = require("./app/routes/routes_medicaltest.js");
+
+var model = require("./app/database/models/index.js");;
+
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-userRoutes(app);
-medTestRoutes(app)
+userRoutes(app, model);
+medTestRoutes(app, model);
 
-var server = app.listen(3000, function () {
+var server = app.listen(3000, function() {
     console.log("app running on port.", server.address().port);
 });
